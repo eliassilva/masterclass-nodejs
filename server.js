b6c0c367f5cca8f3a1eb077e25251c00b94227ca
Elias Silva <eliassilva@eliassilva.me>
2023-04-20 01:11:45 -0300

    http download txt commonjs

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server = express();
+function send404(res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.write('Error 404: Resource not found.');
+  res.end();
+}
 
-server.get('/', (req, res) => {
-  res.send('masterclass nodejs');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    let fileurl;
+    if (req.url === '/') {
+      fileurl = 'log.txt';
+    } else {
+      fileurl = req.url;
+    }
+
+    const filepath = path.resolve(`./${fileurl}`);
+
+    fs.exists(filepath, (exists) => {
+      if (!exists) {
+        send404(res);
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      fs.createReadStream(filepath).pipe(res);
+    });
+  }
 });
 
 server.listen(8080, () => {
